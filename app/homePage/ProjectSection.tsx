413d7b167304b409963fa6e1cb9004ad8bd68171
Sofia Johnsson <sofiaq1@gmail.com>
2026-09-04 12:19:21 +0200

    add tags and hide icon for now

--- app/homePage/ProjectSection.tsx
+++ app/homePage/ProjectSection.tsx
@@ -8,24 +8,28 @@ const projectList = [
     title: "MC-Parking PWA",
     body: "A progressive web app, PWA, that helps motorcyclists find parking in Stockholm City. Written in TypeScript with Next.js utilising Googles Maps JavaScript API and Stockholms Stads LvWS 4 API as well as their Parking API.",
     icon: GhostIcon,
+    tags: ["TypeScript", "Next.js", "PWA"],
   },
   {
     color: "bg-mist-200",
     title: "Photography Portfolio",
     body: "A progressive web app, PWA, that helps motorcyclists find parking in Stockholm City. Written in TypeScript with Next.js utilising Googles Maps JavaScript API and Stockholms Stads LvWS 4 API as well as their Parking API.",
     icon: GhostIcon,
+    tags: ["JavaScript", "Next.js", "PWA", "Tailwind"],
   },
   {
     color: "bg-purple-200",
     title: "Server Chat Web App",
     body: "A PERN-stack web application with channels and servers, running sockets for updating of the chat feeds. The application can be run using docker compose or locally. Contains unit tests and github workflow.",
     icon: GhostIcon,
+    tags: ["React", "Express.js", "PostgreSQL"],
   },
   {
     color: "bg-orange-200",
     title: "Vehicle Maintenance App",
     body: "A React Native + Expo application for registering maintenance on you car och motorcycle. The backend is an Express REST API with a MongoDB database",
     icon: DirectionIcon,
+    tags: ["React Native"],
   },
 ];
 
@@ -36,19 +40,33 @@ export function ProjectSection() {
         <SectionHeader number="02" color="bg-yellow-200">
           projects
         </SectionHeader>
-        <div className="flex gap-10">
+        {/* Project card box */}
+        <div className="flex flex-wrap  gap-10">
           {projectList.map((item) => (
-            <div className="relative flex flex-col items-center w-100 h-50 border-2 border-mauve-800 p-5 pt-8">
+            <div className="relative flex flex-col justify-between items-center max-w-100 border-2 border-mauve-800 p-5 pt-8 gap-6 grow">
               <div
-                className={`absolute -top-5 w-fit h-fit px-8 py-1 ${item.color || "bg-amber-200"}`}
+                className={`absolute -top-5 w-fit h-fit px-8 py-1 bg-mauve-900`}
               >
-                <p className="text-xl">{item.title}</p>
+                <p className="text-xl text-white">{item.title}</p>
               </div>
               <div className="w-full text-lg">
                 <p>{item.body}</p>
-                <div className="absolute -bottom-8 -right-6 h-[160px] w-full max-w-[200px] opacity-100 bg-mist-500">
-                  <img src={item.icon} alt="" />
+              </div>
+
+              <div className="flex flex-row justify-between w-full gap-4">
+                {/* Tags box */}
+                <div className="flex flex-row flex-wrap w-full h-fit gap-2">
+                  {item.tags.map((tag) => (
+                    <div
+                      className={`px-2 rounded-sm ${item.color || "bg-amber-200"}`}
+                    >
+                      <p>{tag}</p>
+                    </div>
+                  ))}
                 </div>
+                {/* <div className="flex w-fit bg-mist-200 p-1">
+                  <img src={item.icon} className="w-20 h-fit" />
+                </div> */}
               </div>
             </div>
           ))}
